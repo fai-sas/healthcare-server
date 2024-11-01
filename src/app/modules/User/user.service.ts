4ab4eeae91397f76f8b84ac28c66c74ceed3c1b2
fai-sas <craftedbyfaisal@gmail.com>
2024-11-01 20:46:42 +0600

    create user and admin service added

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -4,7 +4,27 @@ import * as bcrypt from 'bcrypt'
 const prisma = new PrismaClient()
 
 const createAdminIntoDb = async (data: any) => {
-  return console.log('create admin')
+  const hashedPassword: string = await bcrypt.hash(data.password, 12)
+
+  const userData = {
+    email: data.admin.email,
+    password: hashedPassword,
+    role: UserRole.ADMIN,
+  }
+
+  const result = await prisma.$transaction(async (transactionClient) => {
+    await transactionClient.user.create({
+      data: userData,
+    })
+
+    const createdAdminData = await transactionClient.admin.create({
+      data: data.admin,
+    })
+
+    return createdAdminData
+  })
+
+  return result
 }
 
 export const userService = {
